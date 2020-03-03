96caee0a5dfcca5f8da267edea632ec5b80b0e23
Jose Fernando Garcia <joselino1996@gmail.com>
2020-03-03 20:33:23 +0100

    Parallax_V_1.0

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    public float speed;
+    public Vector2 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        offset.x += speed/10000;
+        offset.x %= 1;
+        GetComponent<Renderer>().material.mainTextureOffset = offset;
+    }
+}
